0d739375a0fcd8911a89bd5e1248a575ff8ba5a3
Kalpana Kathait <KALPANAKATHAIT11@GMAIL.COM>
2023-05-22 01:52:58 +0530

    modified backend

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,53 @@
-import { Server } from "socket.io";
-import { getDocument, updateDocument, updateDocumentName } from "./controller/document.js";
-import { mongoose } from "mongoose";
-import dotenv from 'dotenv'
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import { getDocument, updateDocument, updateDocumentName } from './controller/document.js';
+import { mongoose } from 'mongoose';
+import dotenv from 'dotenv';
+
 dotenv.config();
-const PORT = process.env.PORT || 5000
-console.log(PORT)
-const io = new Server(PORT, {
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000',
-    methods: ['GET', 'POST']
-  }
+    methods: ['GET', 'POST'],
+  },
 });
 
-const MONGODB_URI=process.env.MONGODB_URI
-console.log(MONGODB_URI)
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-io.on('connection', socket => {
-  socket.on('get-document', async documentId => {
+io.on('connection', (socket) => {
+  socket.on('get-document', async (documentId) => {
     const document = await getDocument(documentId);
     socket.join(documentId);
     socket.emit('load-document', document.data);
 
-    socket.on('send-changes', delta => {
+    socket.on('send-changes', (delta) => {
       socket.broadcast.to(documentId).emit('receive-changes', delta);
     });
 
-  socket.on('update-document-name', async(newName) =>{
-        await updateDocumentName(documentId, newName);
-        socket.broadcast.to(documentId).emit('update-document-name', newName)
-  })
+    socket.on('update-document-name', async (newName) => {
+      await updateDocumentName(documentId, newName);
+      socket.broadcast.to(documentId).emit('update-document-name', newName);
+    });
+
     socket.on('save-document', async (data, newName) => {
       await updateDocument(documentId, data);
     });
   });
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
