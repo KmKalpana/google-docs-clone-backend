85e8aac6805276596c364d029f1516f489219f5a
Kalpana Kathait <KALPANAKATHAIT11@GMAIL.COM>
2023-05-22 12:20:50 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,15 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: [process.env.FRONTEND_URL],
     methods: ['GET', 'POST'],
   },
 });
 
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
+console.log(MONGODB_URI);
+console.log(PORT)
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
